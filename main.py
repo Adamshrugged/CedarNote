789e9630282e23d07dc039e9cac43c7be0ed707e
Adamshrugged <adamspivey@gmail.com>
2025-06-24 18:24:46 -0500

    Updated setup to have blank users file by default
    If file doesn't exist, creates it. If it already exists, should ignore it

--- main.py
+++ main.py
@@ -2,15 +2,26 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import os
+import shutil
 from fastapi.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
 
 # --- Basic Configs ---
 from core.templates import templates, get_theme, AVAILABLE_THEMES
-from core.config import NOTES_DIR, ACTIVE_THEME, SECRET_KEY
+from core.config import NOTES_DIR, ACTIVE_THEME, SECRET_KEY, USERS_FILE, SHARED_FILE
 app = FastAPI()
 os.makedirs(NOTES_DIR, exist_ok=True)
+
+
+# Ensure system setup files exist. If not, tries to create it
+def ensure_file_exists(real_file: str, template_file: str):
+    """Create real_file from template_file if it doesn't exist."""
+    if not os.path.exists(real_file):
+        print(f"{real_file} not found. Creating from {template_file}.")
+        shutil.copy(template_file, real_file)
+ensure_file_exists(USERS_FILE, "template.json")
+ensure_file_exists(SHARED_FILE, "template.json")
 
 
 # Templates and static folder
